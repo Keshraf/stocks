6fec99be87de619300395f6ef0a5cee518d13389
Ketan Saraf <saraf.ketan1108@gmail.com>
2023-05-06 02:07:57 +0530

    fix: import

--- src/components/Email/index.tsx
+++ src/components/Email/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Html } from "@react-email/html";
-import { Button } from "@react-email/Button";
+import { Button } from "@react-email/button";
 import { styled } from "stitches.config";
 
 type Props = {
